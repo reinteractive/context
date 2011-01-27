5c7e2916666e364446de70a5f482acdcbc8a7e0d
Jason Stirk <jason.stirk@rubyx.com>
2011-01-27 14:14:46 +1100

    Start loading tags

--- spec/integration/context_spec.rb
+++ spec/integration/context_spec.rb
@@ -1,9 +1,13 @@
 require 'spec_helper'
 
 describe "context()" do
+  before(:all) do
+    Snippet.create!(:name => 'test', :body => 'I am a call to context!')
+  end
+
   it "should return some content" do
     visit context_path
     page.body.should_not be_blank
-    page.body.should =~ /context/
+    page.body.should =~ /I am a call to context!/
   end
 end
--- spec/integration/context_tag_spec.rb
+++ spec/integration/context_tag_spec.rb
@@ -1,10 +1,14 @@
 require 'spec_helper'
 
 describe "context_tag()" do
+  before(:all) do
+    Snippet.create!(:name => 'test', :body => 'I am a call to context!')
+  end
+
   it "should return some content" do
     visit context_tag_path
     page.body.should_not be_blank
     page.body.should =~ /div/
-    page.body.should =~ /context/
+    page.body.should =~ /I am a call to context!/
   end
 end
