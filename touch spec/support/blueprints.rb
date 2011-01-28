3d5ab2fde2324ae7f1a80df57914ee62bf30f13b
Jason Stirk <jason.stirk@rubyx.com>
2011-01-28 12:11:35 +1100

    Remove bogus directory

--- touch spec/support/blueprints.rb
+++
@@ -1,9 +0,0 @@
-require 'machinist/active_record'
-require 'faker'
-require 'sham'
-
-Sham.email { Faker::Internet.email }
-Sham.hostname { Faker::Internet.domain_name }
-Sham.name  { Faker::Name.name }
-Sham.text  { Faker::Lorem.sentence }
-
