85f906ea47069d252226f748bd9550fce056dfb0
Jason Stirk <jason.stirk@rubyx.com>
2011-02-01 16:24:09 +1100

    Page.path is now generated and maintained according to the heirarchy

--- app/models/context/page.rb
+++ app/models/context/page.rb
@@ -1,6 +1,9 @@
 # Page is a specilisation of a Snippet that has a URL slug
 class Context::Page < Context::Snippet
 
+  before_validation :generate_slug_if_blank
+  before_save :update_cached_path
+
   # Returns the layout that should be rendered when shown.
   # This is provided so as that subclasses can force the page to show in a specific layout.
   # For example, if there is a "Faq" class that inherits from Page, it can always show in the "faq" layout by
@@ -9,4 +12,25 @@ class Context::Page < Context::Snippet
   def layout
     true
   end
+
+  # before_save
+  # Updates the path attribute based upon that of the parent
+  def update_cached_path(force_parent_path=nil)
+    if force_parent_path || self.slug_changed? then
+      parent_path = force_parent_path || self.parent.try(:path)
+      self.path=[ parent_path, self.slug ].compact.join('/')
+      self.save if force_parent_path
+      self.children.each do |child|
+        child.update_cached_path(self.path)
+      end
+    end
+  end
+
+private
+  # before_validation
+  # Generates a suitable slug from the name if the slug is left blank
+  def generate_slug_if_blank
+    self.slug = self.name.strip.downcase.gsub(/[^A-Z0-9\-_]/i, '-').gsub(/-{2,}/,'-') if self.slug.blank?
+  end
+
 end
--- spec/models/context/page_spec.rb
+++ spec/models/context/page_spec.rb
@@ -3,8 +3,12 @@ require 'spec_helper'
 describe Context::Snippet do
 
   it "should require a name"
-  it "should require a slug"
-  it "should generate a slug from the parent's slug and it's name if left blank"
+
+  it "should generate a slug it's name if left blank" do
+    @page=Context::Page.create!(:name => 'Test Page', :body => 'I am a test page!', :slug => nil)
+    @page.slug.should == 'test-page'
+    @page.path.should == 'test-page'
+  end
 
   context "instance" do
     before(:each) do
@@ -17,7 +21,35 @@ describe Context::Snippet do
         @page.layout.should === true
       end
     end
+
+    describe ".path" do
+      it "should include the slug" do
+        @page.path.should == "#{@page.slug}"
+      end
+
+      it "should include the slugs of the parents" do
+        parent=Context::Page.create!(:name => "Another Page", :body => "I am another page!", :slug => 'another_page', :parent => @page)
+        child=Context::Page.create!(:name => "Yet Another Page", :body => "I am yet another page!", :slug => 'yet_another_page', :parent => parent)
+        child.path.should == "#{@page.slug}/#{parent.slug}/#{child.slug}"
+      end
+
+      it "should be updated when the slug changes" do
+        old_path=@page.path
+        @page.update_attributes(:slug => 'new-slug-test-page')
+        @page.path.should_not == old_path
+        @page.path.should == "new-slug-test-page"
+      end
+
+      it "should be updated when any parent slugs change" do
+        parent=Context::Page.create!(:name => "Another Page", :body => "I am another page!", :slug => 'another_page', :parent => @page)
+        child=Context::Page.create!(:name => "Yet Another Page", :body => "I am yet another page!", :slug => 'yet_another_page', :parent => parent)
+        old_path=child.path
+        @page.reload
+        @page.update_attributes(:slug => 'new-parent-slug')
+        child.reload
+        child.path.should_not == old_path
+        child.path.should == "new-parent-slug/#{parent.slug}/#{child.slug}"
+      end
+    end
   end
-
 end
-
