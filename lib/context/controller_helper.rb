4e4abc70d075adc341ff21093f898358de5d5629
Ryan Bigg <radarlistener@gmail.com>
2011-02-08 10:56:00 +1100

    Fix indentation for controller helper

--- lib/context/controller_helper.rb
+++ lib/context/controller_helper.rb
@@ -1,21 +1,21 @@
 module Context
-module ControllerHelper
+  module ControllerHelper
 
-  # Returns the current Context::Page for the given path.
-  # Defaults to params[:path] if path is not provided.
-  #
-  # Can be used within custom actions when you still want to load the Context::Page.
-  #
-  #   class NewsController < ApplicationController
-  #     def index
-  #       @page=context_page('news')
-  #       @featured=NewItem.featured.first
-  #     end
-  #   end
-  def context_page(path=nil)
-    # TODO: Should restrict to published pages
-    Page.find_by_path(path ||= params[:path])
+    # Returns the current Context::Page for the given path.
+    # Defaults to params[:path] if path is not provided.
+    #
+    # Can be used within custom actions when you still want to load the Context::Page.
+    #
+    #   class NewsController < ApplicationController
+    #     def index
+    #       @page=context_page('news')
+    #       @featured=NewItem.featured.first
+    #     end
+    #   end
+    def context_page(path=nil)
+      # TODO: Should restrict to published pages
+      Page.find_by_path(path ||= params[:path])
+    end
+
   end
-
-end
 end
