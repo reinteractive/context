37f83456de65c958c8dc60ee7f11b4de32f202c2
Ryan Bigg <radarlistener@gmail.com>
2011-02-08 10:36:58 +1100

    Remove the mock_page method which isn't called within the PagesController spec

--- spec/controllers/context/pages_controller_spec.rb
+++ spec/controllers/context/pages_controller_spec.rb
@@ -2,12 +2,6 @@ require 'spec_helper'
 
 describe Context::PagesController do
 
-  def mock_page(stubs={})
-    (@mock_page ||= mock_model(Context::Page).as_null_object).tap do |page|
-      page.stub(stubs) unless stubs.empty?
-    end
-  end
-
   describe "GET show" do
     it "assigns the page when it is found by path" do
       page=Context::Page.create(:name => 'Foo/Bar Page')
