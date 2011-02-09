38a561719910777bf46819641d801e194e70af1d
Ryan Bigg <radarlistener@gmail.com>
2011-02-09 12:31:37 +1100

    Route for context/admin/pages should probably be made less "open"

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
 
     namespace :admin do
       root :to => "base#index"
-      resources :pages, :constraints => { :id => /.*/ }
+      resources :pages, :constraints => { :id => /.*/ } # TODO: allow for edit route, i.e. /home/edit
     end
 
     resource :pages, :except => :show
