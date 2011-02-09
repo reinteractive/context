8c9d503561bff48a89c14466d43c123a300a3a03
Ryan Bigg <radarlistener@gmail.com>
2011-02-09 15:23:18 +1100

    Add the ability to update a page. Also added tests for invalid pages on create action

--- app/controllers/context/admin/pages_controller.rb
+++ app/controllers/context/admin/pages_controller.rb
@@ -1,14 +1,16 @@
 class Context::Admin::PagesController < Context::Admin::BaseController
+  before_filter :find_page, :only => [:show, :edit, :update, :destroy]
+  
   def index
     @pages = Context::Page.all
   end
   
   def show
-    @page = Context::Page.find(params[:id])
+    
   end
   
   def new
-    @pages = Context::Page.all
+    find_parent_pages
     @page = Context::Page.new
   end
   
@@ -18,8 +20,37 @@ class Context::Admin::PagesController < Context::Admin::BaseController
       flash[:notice] = "A new page has been created."
       redirect_to context_admin_pages_url
     else
+      find_parent_pages
       flash[:error] = "This page could not be created."
       render :action => "new"
     end
+  end
+  
+  def edit
+    find_parent_pages
+  end
+  
+  def update
+    if @page.update_attributes(params[:context_page])
+      flash[:notice] = "This page has been updated."
+      redirect_to context_admin_page_url(@page)
+    else
+      find_parent_pages
+      flash[:error] = "This page could not be updated."
+      render :action => "edit"
+    end
+  end
+  
+  private
+  
+  def find_parent_pages
+    # TODO: Change this to just top-level pages (maybe?)
+    # Used for displaying parent pages that a page could be a child of
+    @pages = Context::Page.all
+  end
+    
+  
+  def find_page
+    @page = Context::Page.find(params[:id])
   end
 end
--- spec/integration/admin/pages_spec.rb
+++ spec/integration/admin/pages_spec.rb
@@ -47,8 +47,33 @@ describe "Admin panel, pages" do
     assert_seen("Showing off stuff from 2010")
   end
   
+  it "cannot create a new page if the name is left blank" do
+    click 'New Page'
+    click 'Create Page'
+    assert_seen("This page could not be created.")
+  end
+  
   it "cannot create a page under root"
     # Doing so will generate an incorrect path of something like //contact-us.
   
+  it "updates a page" do
+    click 'Home'
+    click 'Edit Page'
+    fill_in 'Name', :with => "THE BEST PAGE EVER"
+    click 'Update Page'
+    
+    assert_seen('This page has been updated')
+    page.current_url.should eql("http://www.example.com/context/admin/pages/1")
+    page.should have_css("h2", :text => "THE BEST PAGE EVER")
+    page.should_not have_css("h2", :text => "Home")
+  end
+  
+  it "cannot update a page if the name is left blank" do
+    click 'Home'
+    click 'Edit Page'
+    fill_in 'Name', :with => ""
+    click 'Update Page'
+    assert_seen("This page could not be updated.")
+  end
   
 end
