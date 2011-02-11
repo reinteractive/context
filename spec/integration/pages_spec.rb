f172b9c2fec1a050c0c116030c05cb5ff99d7ac8
Ryan Bigg <radarlistener@gmail.com>
2011-02-11 14:18:55 +1100

    Don't generate custom paths for simple page tests

--- spec/integration/pages_spec.rb
+++ spec/integration/pages_spec.rb
@@ -3,13 +3,12 @@ require 'spec_helper'
 describe "Pages" do
   before(:each) do
     @page=Context::Page.create!(:name      => 'Test Page', 
-                                :body      => 'I am a test page!', 
-                                :slug      => 'test_page',
+                                :body      => 'I am a test page!',
                                 :published => true)
   end
 
   it "should render correctly from the slug" do
-    visit "/test_page"
+    visit "/test-page"
     page.body.should_not be_blank
     # Make sure we are in the application layout
     page.body.should =~ /Context Test Layout/
@@ -18,8 +17,8 @@ describe "Pages" do
 
   it "should render with a custom layout if set by the subclass" do
     @page.should_receive(:layout).and_return('custom')
-    Context::Page.should_receive(:find_by_path).with('test_page').and_return(@page)
-    visit "/test_page"
+    Context::Page.should_receive(:find_by_path).with('test-page').and_return(@page)
+    visit "/test-page"
     page.body.should_not be_blank
     # Make sure we are in the custom layout
     page.body.should =~ /Custom Layout/
@@ -43,12 +42,12 @@ describe "Pages" do
 
   it "should use Context::Configuration.page_show_template_path to render a different view template" do
     Context::Configuration.should_receive(:page_show_template_path).twice.and_return(File.join(Rails.root, 'app', 'views', 'test', 'custom_show.html.erb'))
-    visit "/test_page"
+    visit "/test-page"
     page.body.should =~ /CUSTOM VIEW TEMPLATE/
   end
   
   it "generates the correct path" do
-    @page.path.should eql("test_page")
+    @page.path.should eql("test-page")
   end
   
   it "allows for custom page paths" do
