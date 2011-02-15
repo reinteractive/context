7fbcba08df28f9ab83a287514da500776bade509
Jason Stirk <jason.stirk@rubyx.com>
2011-02-15 22:01:59 +1100

    Cleanup unnecessary files

--- config/environment.rb
+++ config/environment.rb
@@ -1,6 +1,2 @@
 # Load the rails application
 require File.expand_path('../../lib/context', __FILE__)
-
-# NOTE: Doesn't appear to be necessary?
-# Initialize the rails application
-#Context::Engine.initialize!
